c6ec00c93c48d15f3188f213359c71fb60c5cfc8
headhunteerr <niki6_d2@hotmail.fr>
2017-11-26 00:20:23 +0100

    Commit

---
+++ SauverTP/Assets/Scripts/IntroPanel.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class IntroPanel : MonoBehaviour{
+
+    public Button startButton;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    public void GameStart(){
+        StartCoroutine(waitToRead());
+        startButton.gameObject.SetActive(true);
+    }
+
+    IEnumerator waitToRead()
+    {
+        yield return new WaitForSeconds(5f);
+    }
+}
